be8f6bfac3d291fda3b30a89b5560d688aa5ad85
TKP30 <hahaguo1107@qq.com>
2025-08-10 16:34:28 +0800

    添加SQLite接口，等待具体实现

---
+++ scripts/sqlite.js
@@ -0,0 +1,36 @@
+// @/scripts/sqlite.js
+
+export async function loadDB() {
+	await plus.sqlite.openDatabase({
+		name: 'railgo',
+		path: '_doc/railgo.sqlite',
+		fail: () => {
+			console.log("Error when loading SQLite database");
+		}
+	});
+}
+
+export async function doQuery(sql, callback) {
+	if (!await plus.sqlite.isOpenDatabase({
+			name: 'railgo',
+			path: '_doc/railgo.db'
+		})) {
+		throw new Error("SQLite DB hasn't opened yet");
+	}
+	await plus.sqlite.selectSql({
+		name: 'railgo',
+		sql: sql,
+		success: data => {
+			callback(data);
+		},
+		fail: () => {
+			throw new Error("Failed to execute the SQL");
+		}
+	});
+}
+
+export async function close() {
+	await plus.sqlite.closeDatabase({
+		name: "railgo"
+	});
+}
